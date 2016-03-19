15d97c0a1f65d37b00057d5d734382f150c2288f
Sanjid Halim <Sanjid Halim>
2016-03-19 14:19:55 -0500

    Camera added

--- imifai/app/src/main/java/com/example/android/imifai/Camera.java
+++ imifai/app/src/main/java/com/example/android/imifai/Camera.java
@@ -1,13 +1,35 @@
 package com.example.android.imifai;
 
+import android.content.Intent;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class Camera extends AppCompatActivity {
+    static final int REQUEST_IMAGE_CAPTURE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera);
+        dispatchTakePictureIntent();
+
+    }
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Log.d("CAMERA", extras.toString());
+
+        }
     }
 }
--- imifai/app/src/main/java/com/example/android/imifai/MainActivity.java
+++ imifai/app/src/main/java/com/example/android/imifai/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.android.imifai;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -26,6 +27,9 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        Intent intent = new Intent(this, Camera.class);
+        startActivity(intent);
     }
 
     @Override
