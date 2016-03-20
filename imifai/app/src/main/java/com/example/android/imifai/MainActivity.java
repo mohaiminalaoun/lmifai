cefb490dd6d508c3ad65f301aef9555efcf41216
Sanjid Halim <Sanjid Halim>
2016-03-20 02:08:23 -0500

    Search bar

--- imifai/app/src/main/java/com/example/android/imifai/MainActivity.java
+++ imifai/app/src/main/java/com/example/android/imifai/MainActivity.java
@@ -58,7 +58,7 @@ public class MainActivity extends AppCompatActivity {
 
     private void startCamera(){
 
-        Intent intent  = new Intent(this, Camera.class);
+        Intent intent  = new Intent(this, Search.class);
 
         startActivity(intent);
     }
